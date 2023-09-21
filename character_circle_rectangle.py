30870e4bd0ce47d3d0a0741cd1cd4ae7f507f930
joozion <joozion@naver.com>
2023-09-21 09:48:14 +0900

    Drill02_new start

---
+++ character_circle_rectangle.py
@@ -0,0 +1,11 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+close_canvas()
