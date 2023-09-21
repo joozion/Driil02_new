4d8252d1e78e4791530886c366dc655bfafe2dcd
joozion <joozion@naver.com>
2023-09-21 09:58:38 +0900

    circle, rectangle root add

--- character_circle_rectangle.py
+++ character_circle_rectangle.py
@@ -8,4 +8,16 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+whild True:
+    run_circle()
+    run_rectangle()
+
 close_canvas()
